fecd8c1cb0c3f1825b5d6bcd6ee49707193c9770
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-28 02:27:41 +0300

    feat: добавить логирование запросов и ошибок

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const mainRouter = require('./routes/index');
 const catchErrors = require('./middlewares/errors');
 
@@ -9,7 +10,9 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.use(mainRouter);
+app.use(errorLogger);
 app.use(catchErrors);
 
 mongoose.connect('mongodb://localhost:27017/moviesdb');
