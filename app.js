7dd036de1fc161a0552ce9270b029d3e1377f683
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-28 03:15:12 +0300

    feat: добавить rate limiter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const rateLimiter = require('./middlewares/rateLimiter');
 const { DB_URL } = require('./utils/serverConfigs');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const mainRouter = require('./routes/index');
@@ -12,6 +13,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
+app.use(rateLimiter);
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(mainRouter);
