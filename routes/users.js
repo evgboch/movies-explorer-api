51c9d52354a39c0e47e8139b369f8a273e8f8e0e
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-29 18:19:53 +0300

    fix: удалить валидацию поля password из роута обновления информации пользователя

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ router.get('/me', getUserInfo);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required(),
     name: Joi.string().required().min(2).max(30),
   }),
 }), updateUserInfo);
