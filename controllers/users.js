635c3decb04daec4bd7969bee20b4f43115e283c
Evgeny Bocharov <evgbocharov@gmail.com>
2022-10-29 18:27:45 +0300

    feat: добавить обработку ошибки существующей почты при редактировании данных пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -88,6 +88,8 @@ function updateUserInfo(req, res, next) {
     .catch((err) => {
       if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
         next(new BadRequestError());
+      } else if (err.code === 11000) {
+        next(new ConflictError());
       } else {
         next(err);
       }
